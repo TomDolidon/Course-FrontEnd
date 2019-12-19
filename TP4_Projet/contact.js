0e1a31ceeac4e39459c087f52c03a238b24dcace
TomDolidon <dolidon.tom@gmail.com>
2019-12-19 20:03:02 +0100

    Add files via upload
    init commit

---
+++ TP4_Projet/contact.js
@@ -0,0 +1,21 @@
+function checkAuth() {
+
+
+   /* $.ajax({
+        url: "connexion.php",
+        type:"POST"})
+        .done(function( arg) {
+            console.log("reussi");
+            
+         //   alert( "Données : " + arg); 
+        }); */
+
+     /*   $.ajax({
+            type: 'POST',
+            url: "connexion.php",
+            data: postedData,
+            dataType: 'json',
+            success: callback
+          }); */
+
+}
